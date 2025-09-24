6d8403f5444d3aeb14b6adf0db08fb39240333b7
sujaiunzer <sujai.karunakaran@unzer.com>
2025-09-24 21:32:24 +0200

    first commit

---
+++ adsquare/src/test/java/com/sujai/adsquare/AdsquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujai.adsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
